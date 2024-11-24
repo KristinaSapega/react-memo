6d7581ee863f6dd03bab8ac8cf18a81f24d3b11e
Kristina <tinasapega@gmail.com>
2024-11-24 21:57:42 +0300

    handleStartGame added & [selectedLevel, setSelectedLevel]

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -1,30 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./SelectLevelPage.module.css";
 import { useGameMode } from "../../contexts/GameModeContext";
 import { Button } from "../../components/Button/Button";
+import { useState } from "react";
 
 export function SelectLevelPage() {
   const { mistakeMode, setMistakeMode } = useGameMode();
+  const [selectedLevel, setSelectedLevel] = useState(null);
+  const navigate = useNavigate();
+
+  const handleStartGame = () => {
+    if (selectedLevel) {
+      navigate(`/game/${selectedLevel}`);
+    } else {
+      alert("Пожалуйста, выберите уровень сложности!");
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
         <h1 className={styles.title}>Выбери сложность</h1>
         <ul className={styles.levels}>
-          <li className={styles.level}>
-            <Link className={styles.levelLink} to={`/game/3`}>
-              1
-            </Link>
+          <li
+            className={`${styles.level} ${selectedLevel === 3 ? styles.active : ""}`}
+            onClick={() => setSelectedLevel(3)}
+          >
+            1
           </li>
-          <li className={styles.level}>
-            <Link className={styles.levelLink} to={`/game/6`}>
-              2
-            </Link>
+          <li
+            className={`${styles.level} ${selectedLevel === 6 ? styles.active : ""}`}
+            onClick={() => setSelectedLevel(6)}
+          >
+            2
           </li>
-          <li className={styles.level}>
-            <Link className={styles.levelLink} to={`/game/9`}>
-              3
-            </Link>
+          <li
+            className={`${styles.level} ${selectedLevel === 9 ? styles.active : ""}`}
+            onClick={() => setSelectedLevel(9)}
+          >
+            3
           </li>
         </ul>
         {/* <div className={styles.box}> */}
@@ -37,7 +51,7 @@ export function SelectLevelPage() {
           />
           Легкий режим (3 жизни)
         </label>
-        <Button>Играть</Button>
+        <Button onClick={handleStartGame}>Играть</Button>
         <div className={styles.leaderboard}>
           <Link className={styles.leaderboardLink} to="/leaderboard">
             Перейти к лидерборду
